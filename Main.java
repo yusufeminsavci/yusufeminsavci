c055fe69564b6fdd80bd3adf1b8a3545169b047c
Yusuf Emin Savcı <yusufyatagan@gmail.com>
2022-09-17 21:11:51 +0300

    Add files via upload
    kullanıcıların girdiği sayıya göre çarpım tablosu işlemleri

--- Main.java
+++ Main.java
@@ -1,45 +1,27 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Değişkenlerimizi tanımlıyoruz.
-        double n1, n2;
-        int select;
 
-        // Kullanıcıdan değer alıyoruz
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Please enter first number: ");
-        n1 = inp.nextDouble();
-        System.out.print("Please enter second number: ");
-        n2 = inp.nextDouble();
+       Scanner scanner = new Scanner(System.in);
 
-        System.out.print(
-                "For sum:1\nFor subtraction:2\nFor multiplication: 3\nFor divide: 4\nSelect one of them: ");
-        select = inp.nextInt();
-        switch (select) {
-            case 1:
-                System.out.println("Your result: " + (n1 + n2));
-                break;
-            case 2:
-                System.out.println("Your result: " + (n1 - n2));
-                break;
-            case 3:
-                System.out.println("Your result: " + (n1 * n2));
-                break;
-            case 4:
-                if (n2 == 0) {
-                    System.out.println("A number cannot be divided by zero!");
-                } else {
-                    System.out.println("Your result: " + (n1 / n2));
-                }
-                break;
-            default:
-                System.out.println("Out of number! Please enter valid number!");
-                break;
+       System.out.print("Bir Sayı Giriniz : ");
+       int sayi=scanner.nextInt();
+       for (int i=1;i<=10;i++){
+           System.out.println(sayi+"*"+i+"="+sayi*i);;
+
+       }
+
+
+
+
+
         }
     }
-}
+
+
+
 
 
 
