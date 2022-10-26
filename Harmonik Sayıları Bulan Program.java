75be1d72a7bf71a29565fb0c79f92510723221b9
Yusuf Emin Savcı <yusufyatagan@gmail.com>
2022-10-26 22:36:13 +0300

    Add files via upload

---
+++ Harmonik Sayıları Bulan Program.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("N sayısını giriniz : ");
+        int n = input.nextInt();
+        double result = 0.0;
+        for (double i = 1; i <= n ; i++) {
+            result += (1/i);
+        }
+        // 1. Döngü, i = 1; result = 0 + (1/1) = 1;
+        // 2. Döngü, i = 2; result = 1 + (1/2)= 1.5;
+
+        System.out.println(result);
+    }
+}
